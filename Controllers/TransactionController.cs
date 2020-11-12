13d7ec962f49d84a6e102852c9fbe1600e448111
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-11-12 19:48:27 +0530

    first com

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using core_crud_mvc.Models;
+
+
+namespace core_crud_mvc.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+               
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new TransactionModel());
+            else
+            {
+                var transactionModel = await _context.Transactions.FindAsync(id);
+                if (transactionModel == null)
+                {
+                    return NotFound();
+                }
+                return View(transactionModel);
+            }
+        }
+
+        [HttpPost]        
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //Insert
+                if (id == 0)
+                {
+                    transactionModel.Date = DateTime.Now;
+                    _context.Add(transactionModel);
+                    await _context.SaveChangesAsync();
+
+                }
+                //Update
+                else
+                {
+                    try
+                    {
+                        _context.Update(transactionModel);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!TransactionModelExists(transactionModel.TransactionId))
+                        { return NotFound(); }
+                        else
+                        { throw; }
+                    }
+                }
+                return RedirectToAction(nameof(GetAllEmployees));
+            }
+            return RedirectToAction(nameof(Error))
+        }
+       
+        public Task<IActionResult> Error(){
+
+        	return View();
+        } 
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            _context.Transactions.Remove(transactionModel);
+            await _context.SaveChangesAsync();
+            return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Transactions.ToList()) });
+        }
+
+        private bool TransactionModelExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionId == id);
+        }
+    }
+}
